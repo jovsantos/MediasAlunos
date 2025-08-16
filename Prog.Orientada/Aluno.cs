49f7c4950cb27e97a4f2eba7b43a26d72f64f00d
conta <conta@JOAOZIN>
2025-08-16 11:31:06 -0300

    Adicionar arquivos de projeto.

---
+++ Prog.Orientada/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog.Orientada
+{
+    public class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+
+        public void ExibirInformações()
+        {
+            Console.WriteLine($"Nome do Aluno: {nome} | Notas: {nota1} e {nota2}");
+        }
+        public double CalcularMedia()
+        {
+            return (nota1 + nota2) / 2;
+            
+        }
+        
+    }
+}
---
+++ Prog.Orientada/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Prog.Orientada;
+
+ public class Program
+{
+    static void Main()
+    {
+        Aluno[] sala = new Aluno[3];
+
+              sala[0] = new Aluno();
+
+        Console.Write("Digite o nome do aluno: ");
+             sala[0].nome = Console.ReadLine();
+        Console.Write("Digite a nota 1: ");
+             sala[0].nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a nota 2: ");
+             sala[0].nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Clear();
+
+        sala[1] = new Aluno();
+
+        Console.Write("Digite o nome do aluno: ");
+             sala[1].nome = Console.ReadLine();
+        Console.Write("Digite a nota 1: ");
+             sala[1].nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a nota 2: ");
+             sala[1].nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Clear();
+
+        sala[2] = new Aluno();
+
+        Console.Write("Digite o nome do aluno: ");
+              sala[2].nome = Console.ReadLine();
+        Console.Write("Digite a nota 1: ");
+              sala[2].nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a nota 2: ");
+              sala[2].nota2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Clear();
+
+        Console.WriteLine("Lista de Alunos: ");
+
+        for (int i = 0; i < sala.Length; i++)
+        {
+            sala[i].ExibirInformações();
+            double media = sala[i].CalcularMedia();
+            Console.WriteLine($"Média: {media}");  
+         
+            if (media >= 6)
+            {
+                Console.WriteLine("Parabéns, você está APROVADO!"); 
+            }
+            else if (media >= 4)
+            {
+                Console.WriteLine("RECUPERAÇÃO, não desanime!");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente, você foi REPROVADO!");
+            }
+        }
+    }  
+}
